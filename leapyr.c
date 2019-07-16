771dc09a8d904cfdc1fe371b7e1b5425587ab88c
rambala99 <51699733+rambala99@users.noreply.github.com>
2019-07-16 09:57:31 +0530

    Create leapyr.c

---
+++ leapyr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%100==0)
+{
+if(a%400==0)
+printf("yes");
+else
+printf("no");
+}
+else
+{
+if(a%4==0)
+printf("yes");
+else
+printf("no");
+}
+}
